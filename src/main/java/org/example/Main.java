c2d92678fd05c44710b264c4a249be789d3cf4b5
KEN21818 <francis.mkalama@gmail.com>
2023-02-08 19:16:52 +0300

    Commit to the created branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    public String processListofString(
+            List<String> list ){
+        return "";
     }
 }
